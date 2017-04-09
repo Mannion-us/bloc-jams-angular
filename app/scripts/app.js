3a3b60008859cac59506787dc918a88761bb37a4
Thomas Mannion <tmannion@gmail.com>
2017-04-09 14:20:57 -0400

    Configured Paths with '' for #! clean URLs...

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,11 @@
 <!--	Providers are services used by Angular modules to either configure or define default behavior for a certain Angular module.  -->
 (function() {
 	function config($stateProvider, $locationProvider) {
+		$locationProvider	//<!--	Configure paths  -->//
+			.html5Mode({
+				enabled: true,
+				requireBase: false
+			});
 	}
 	angular
 		.module('blocJams', ['ui.router'])
