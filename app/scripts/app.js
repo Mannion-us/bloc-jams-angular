c645f27ac9f1b06ca0b27c8348fed6b5139343f3
Thomas Mannion <tmannion@gmail.com>
2017-04-09 14:35:40 -0400

    Configured States with '' on .state 'landing'...

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,22 @@
 <!--	Providers are services used by Angular modules to either configure or define default behavior for a certain Angular module.  -->
 (function() {
+
 	function config($stateProvider, $locationProvider) {
+
 		$locationProvider	//<!--	Configure paths  -->//
 			.html5Mode({
-				enabled: true,
+				enabled: true,	//<!--  Toggle this to hid URLs paths  -->//	//<!--  Do not toggle or will error in console  -->//
 				requireBase: false
 			});
+
+		$stateProvider
+			.state('landing', {
+				url: '/',
+				templateUrl: '/templates/landing.html'
+			});
+
 	}
+
 	angular
 		.module('blocJams', ['ui.router'])
 		.config(config);
