e81fefe50522731467fc55c099f1bcd2389ab5f2
Thomas Mannion <tmannion@gmail.com>
2017-04-09 13:56:42 -0400

    Configured the Module with 'Providers'...

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,8 @@
-angular.module('blocJams',['ui.router']);
+<!--	Providers are services used by Angular modules to either configure or define default behavior for a certain Angular module.  -->
+(function() {
+	function config($stateProvider, $locationProvider) {
+	}
+	angular
+		.module('blocJams', ['ui.router'])
+		.config(config);
+	})();
