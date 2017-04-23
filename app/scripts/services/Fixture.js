c67084adc48c5f8e28f2083e3122507b550d287b
Thomas Mannion <tmannion@gmail.com>
2017-04-23 12:45:46 -0400

    Corrected file path for '/assets' including 'extra' URLs not included in 'curriculum content'...

--- app/scripts/services/Fixture.js
+++ app/scripts/services/Fixture.js
@@ -7,13 +7,13 @@
 			artist: 'Pablo Picasso',
 			label: 'Cubism',
 			year: '1881',
-			albumArtUrl: 'assets/images/album_covers/01.png',
+			albumArtUrl: '/assets/images/album_covers/01.png',
 			songs: [
-				{ title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
-				{ title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
-				{ title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
-				{ title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
-				{ title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+				{ title: 'Blue', duration: 161.71, audioUrl: '/assets/music/blue' },
+				{ title: 'Green', duration: 103.96, audioUrl: '/assets/music/green' },
+				{ title: 'Red', duration: 268.45, audioUrl: '/assets/music/red' },
+				{ title: 'Pink', duration: 153.14, audioUrl: '/assets/music/pink' },
+				{ title: 'Magenta', duration: 374.22, audioUrl: '/assets/music/magenta' }
 			]
 		};
 
@@ -22,20 +22,20 @@
 			artist: 'Elvis Presley',
 			label: 'RCA Victor',
 			year: '1956',
-			albumArtUrl: 'assets/images/album_covers/rca-ep-1382.jpg',
+			albumArtUrl: '/assets/images/album_covers/rca-ep-1382.jpg',
 			songs: [
-				{ title: 'Rip It Up', duration: 107.22, audioUrl: 'assets/music/Rip It Up'  },
-				{ title: 'Love Me', duration: 154.45, audioUrl: 'assets/music/Love Me' },
-				{ title: 'When My Blue Moon Turns to Gold Again', duration: 133.79, audioUrl: 'assets/music/When My Blue Moon Turns to Gold Again' },
-				{ title: 'Long Tall Sally', duration: 105.61, audioUrl: 'assets/music/Long Tall Sally' },
-				{ title: 'First in Line', duration: 193.4, audioUrl: 'assets/music/First in Line' },
-				{ title: 'Paralyzed', duration: 135.78, audioUrl: 'assets/music/Paralyzed' },
-				{ title: 'So Glad You\'re Mine', duration: 134.87, audioUrl: 'assets/music/So Glad You\'re Mine' },
-				{ title: 'Old Shep', duration: 221.7, audioUrl: 'assets/music/Old Shep' },
-				{ title: 'Ready Teddy', duration: 114.17, audioUrl: 'assets/music/Ready Teddy' },
-				{ title: 'Anyplace is Paradise', duration: 139.51, audioUrl: 'assets/music/Anyplace is Paradise' },
-				{ title: 'How\'s the World Treating You', duration: 139.73, audioUrl: 'assets/music/How\'s the World Treating You' },
-				{ title: 'How Do You Think I Feel', duration: 122.78, audioUrl: 'assets/music/How Do You Think I Feel' }
+				{ title: 'Rip It Up', duration: 107.22, audioUrl: '/assets/music/Rip It Up'  },
+				{ title: 'Love Me', duration: 154.45, audioUrl: '/assets/music/Love Me' },
+				{ title: 'When My Blue Moon Turns to Gold Again', duration: 133.79, audioUrl: '/assets/music/When My Blue Moon Turns to Gold Again' },
+				{ title: 'Long Tall Sally', duration: 105.61, audioUrl: '/assets/music/Long Tall Sally' },
+				{ title: 'First in Line', duration: 193.4, audioUrl: '/assets/music/First in Line' },
+				{ title: 'Paralyzed', duration: 135.78, audioUrl: '/assets/music/Paralyzed' },
+				{ title: 'So Glad You\'re Mine', duration: 134.87, audioUrl: '/assets/music/So Glad You\'re Mine' },
+				{ title: 'Old Shep', duration: 221.7, audioUrl: '/assets/music/Old Shep' },
+				{ title: 'Ready Teddy', duration: 114.17, audioUrl: '/assets/music/Ready Teddy' },
+				{ title: 'Anyplace is Paradise', duration: 139.51, audioUrl: '/assets/music/Anyplace is Paradise' },
+				{ title: 'How\'s the World Treating You', duration: 139.73, audioUrl: '/assets/music/How\'s the World Treating You' },
+				{ title: 'How Do You Think I Feel', duration: 122.78, audioUrl: '/assets/music/How Do You Think I Feel' }
 			]
 		};
 
@@ -44,28 +44,28 @@
 			artist: 'Jim Nabors',
 			label: 'Columbia',
 			year: '1972',
-			albumArtUrl: 'assets/images/album_covers/jim-nabors-sings-the-great-love-songs.jpg',
+			albumArtUrl: '/assets/images/album_covers/jim-nabors-sings-the-great-love-songs.jpg',
 			songs: [
-				{ title: 'What Now My Love', duration: 167.23, audioUrl: 'assets/music/What Now My Love' },
-				{ title: 'On A Clear Day You Can See Forever', duration: 203.85, audioUrl: 'assets/music/On A Clear Day You Can See Forever' },
-				{ title: 'Strangers In The Night', duration: 192.14, audioUrl: 'assets/music/Strangers In The Night' },
-				{ title: 'Love Me With All Your Heart', duration: 155.82, audioUrl: 'assets/music/Love Me With All Your Heart' },
-				{ title: 'Somewhere, My Love (Lara\'s Theme)', duration: 109.08, audioUrl: 'assets/music/Somewhere, My Love (Lara\'s Theme)' },
-				{ title: 'Love is Blue', duration: 172.53, audioUrl: 'assets/music/Love Is Blue' },
-				{ title: 'I Must Have Been Out of My Mind', duration: 146.02, audioUrl: 'assets/music/I Must Have Been Out of My Mind' },
-				{ title: 'I Will Wait For You', duration: 145.56, audioUrl: 'assets/music/I Will Wait For You' },
-				{ title: 'By The Time I Get to Phoenix', duration: 163.1, audioUrl: 'assets/music/By The Time I Get to Phoenix' },
-				{ title: 'Born Free', duration: 152.77, audioUrl: 'assets/music/Born Free' },
-				{ title: 'Jean', duration: 212.33, audioUrl: 'assets/music/Jean' },
-				{ title: 'A Time For Us', duration: 169.44, audioUrl: 'assets/music/A Time For Us' },
-				{ title: 'The Windmills of Your Mind', duration: 225.53, audioUrl: 'assets/music/The Windmills of Your Mind' },
-				{ title: 'Try to Remember', duration: 171.49, audioUrl: 'assets/music/Try to Remember' },
-				{ title: 'Something', duration: 160.17, audioUrl: 'assets/music/Something' },
-				{ title: 'It Hurts To Say Goodbye', duration: 116.44, audioUrl: 'assets/music/It Hurts To Say Goodbye' },
-				{ title: 'You Don\'t Have To Say You Love Me', duration: 154.22, audioUrl: 'assets/music/You Don\'t Have To Say You Love Me' },
-				{ title: 'For Once in My Life', duration: 126.26, audioUrl: 'assets/music/For Once in My Life' },
-				{ title: 'Didn\'t We', duration: 166.3, audioUrl: 'assets/music/Didn\'t We' },
-				{ title: 'You Gave Me a Mountain', duration: 227.7, audioUrl: 'assets/music/You Gave Me a Mountain' },
+				{ title: 'What Now My Love', duration: 167.23, audioUrl: '/assets/music/What Now My Love' },
+				{ title: 'On A Clear Day You Can See Forever', duration: 203.85, audioUrl: '/assets/music/On A Clear Day You Can See Forever' },
+				{ title: 'Strangers In The Night', duration: 192.14, audioUrl: '/assets/music/Strangers In The Night' },
+				{ title: 'Love Me With All Your Heart', duration: 155.82, audioUrl: '/assets/music/Love Me With All Your Heart' },
+				{ title: 'Somewhere, My Love (Lara\'s Theme)', duration: 109.08, audioUrl: '/assets/music/Somewhere, My Love (Lara\'s Theme)' },
+				{ title: 'Love is Blue', duration: 172.53, audioUrl: '/assets/music/Love Is Blue' },
+				{ title: 'I Must Have Been Out of My Mind', duration: 146.02, audioUrl: '/assets/music/I Must Have Been Out of My Mind' },
+				{ title: 'I Will Wait For You', duration: 145.56, audioUrl: '/assets/music/I Will Wait For You' },
+				{ title: 'By The Time I Get to Phoenix', duration: 163.1, audioUrl: '/assets/music/By The Time I Get to Phoenix' },
+				{ title: 'Born Free', duration: 152.77, audioUrl: '/assets/music/Born Free' },
+				{ title: 'Jean', duration: 212.33, audioUrl: '/assets/music/Jean' },
+				{ title: 'A Time For Us', duration: 169.44, audioUrl: '/assets/music/A Time For Us' },
+				{ title: 'The Windmills of Your Mind', duration: 225.53, audioUrl: '/assets/music/The Windmills of Your Mind' },
+				{ title: 'Try to Remember', duration: 171.49, audioUrl: '/assets/music/Try to Remember' },
+				{ title: 'Something', duration: 160.17, audioUrl: '/assets/music/Something' },
+				{ title: 'It Hurts To Say Goodbye', duration: 116.44, audioUrl: '/assets/music/It Hurts To Say Goodbye' },
+				{ title: 'You Don\'t Have To Say You Love Me', duration: 154.22, audioUrl: '/assets/music/You Don\'t Have To Say You Love Me' },
+				{ title: 'For Once in My Life', duration: 126.26, audioUrl: '/assets/music/For Once in My Life' },
+				{ title: 'Didn\'t We', duration: 166.3, audioUrl: '/assets/music/Didn\'t We' },
+				{ title: 'You Gave Me a Mountain', duration: 227.7, audioUrl: '/assets/music/You Gave Me a Mountain' },
 			]
 		};
 
@@ -74,13 +74,13 @@
 			artist: 'Guglielmo Marconi',
 			label: 'EM',
 			year: '1909',
-			albumArtUrl: 'assets/images/album_covers/20.png',
+			albumArtUrl: '/assets/images/album_covers/20.png',
 			songs: [
-				{ title: 'Hello, Operator?', duration: 0.0, audioUrl: 'assets/music/ff-strike3' },
-				{ title: 'Ring, ring, ring', duration: 0.0, audioUrl: 'assets/music/ff-strike3' },
-				{ title: 'Fits in your pocket', duration: 0.0, audioUrl: 'assets/music/ff-strike3' },
-				{ title: 'Can you hear me now?', duration: 0.0, audioUrl: 'assets/music/ff-strike3' },
-				{ title: 'Wrong phone number', duration: 0.0, audioUrl: 'assets/music/ff-strike3' }
+				{ title: 'Hello, Operator?', duration: 0.0, audioUrl: '/assets/music/ff-strike3' },
+				{ title: 'Ring, ring, ring', duration: 0.0, audioUrl: '/assets/music/ff-strike3' },
+				{ title: 'Fits in your pocket', duration: 0.0, audioUrl: '/assets/music/ff-strike3' },
+				{ title: 'Can you hear me now?', duration: 0.0, audioUrl: '/assets/music/ff-strike3' },
+				{ title: 'Wrong phone number', duration: 0.0, audioUrl: '/assets/music/ff-strike3' }
 			]
 		};
 
