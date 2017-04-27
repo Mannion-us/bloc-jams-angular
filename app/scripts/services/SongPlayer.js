59af6e9859da37bcc34d5b1faef60b6477526444
Thomas Mannion <tmannion@gmail.com>
2017-04-27 12:12:15 -0400

    Edit 'SongPlayer.play' function to replace excerpted code with call for 'setSong' and other minor adjustments...

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,55 +1,35 @@
 (function() {
 	function SongPlayer() {
 		var SongPlayer = {};
-
 		var currentSong = null;
 		var currentBuzzObject = null;
-
 		var setSong = function(song) {
 			if (currentBuzzObject) {
 				currentBuzzObject.stop();
 				currentBuzzObject.playing = null;
 			}
-
 			currentBuzzObject = new buzz.sound(song.audioUrl,{
 				formats: ['mp3'],
 				preload: true
 			});
-
 			currentSong = song;
 		};
-
 		SongPlayer.play = function(song) {
-
 			if (currentSong !== song) {
-				if (currentBuzzObject) {
-					currentBuzzObject.stop();
-					currentSong.playing = null;
-				}
-
+				setSong(song);
+				currentBuzzObject.play();
+				song.playing = true;
+			}
 			else if (currentSong === song) {
 				if (currentBuzzObject.isPaused()) {
 					currentBuzzObject.play();
 				}
 			}
-
-			currentBuzzObject = new buzz.sound(song.audioUrl, {
-				formats: ['mp3'],
-				preload: true
-			});
-
-			currentSong = song;
-			currentBuzzObject.play();
-			song.playing = true;
-
-			}
 		};
-
 		SongPlayer.pause = function(song) {
 			currentBuzzObject.pause();
 			song.playing = false;
 		};
-
 		return SongPlayer;
 	}
 
