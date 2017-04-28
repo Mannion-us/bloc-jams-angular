418388e0f73be8b9b24f20f03db2d0c133134650
Thomas Mannion <tmannion@gmail.com>
2017-04-28 14:56:06 -0400

    Added documentation for private 'setSong' function to 'SongPlayer.js' file...

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,6 +3,11 @@
 		var SongPlayer = {};
 		var currentSong = null;
 		var currentBuzzObject = null;
+/**
+* @function setSong
+* @desc Stops currently playing song and loads new audio file as currentBuzzObject
+* @param {Object} song
+*/
 		var setSong = function(song) {
 			if (currentBuzzObject) {
 				currentBuzzObject.stop();
