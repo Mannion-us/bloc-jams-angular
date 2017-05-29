c77545ab02e496c66f5be1069f6539b9a82beeb1
Thomas Mannion <tmannion@gmail.com>
2017-05-29 15:53:09 -0400

    Created custom event '.' to be added to 'SongPlayer.setSong' method within 'SongPlayer.js' file...

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -29,6 +29,13 @@
 				formats: ['mp3'],
 				preload: true
 			});
+
+			currentBuzzObject.bind('timeupdate', function() {
+				$rootScope.$apply(function() {
+					SongPlayer.currentTime = currentBuzzObject.getTime();
+				});
+			});
+
 			SongPlayer.currentSong = song;
 		};
 /**
