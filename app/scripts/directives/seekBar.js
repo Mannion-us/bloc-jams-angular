e2175792739dcdaccd0b0c52213433c099c3eb71
Thomas Mannion <tmannion@gmail.com>
2017-05-31 16:32:34 -0400

    Final NOTES for assignment completion to be sent to mentor by way of 'comments' within 'seekBar.js' file...

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -79,3 +79,31 @@
 		.module('blocJams')
 		.directive('seekBar', ['$document', seekBar]);
 })();
+
+/***************************************************************************
+*
+*  Item #1 of the assignment was completed without any need to 'format' 
+* the 'value' for displaying the 'element'.
+*
+*  Item #2 of the assignment was also completed without any need to address 
+* 'a future feature' of 'onChange()' similar to an earlier defined directive 
+* element attribute.
+*
+*  Item #3 of the assignment was clarified to mean the 'arguments' passed 
+* into the directives elements need to be reflective in exact detail to 
+* those 'keys' originating from the passed function.  The term 'hash' meaning 
+* a 'key': 'value' pairing representative of an angular 'object', not 
+* necessarily the entire 'object' but rather a single pairing or more than 
+* one pairing.  They simply need to be consistently passed to avoid being 
+* 'mis-compiled' thus failing to be 'defined' and producing 'errors'.
+*
+*  Item #4 of the assignment indicates that a 'dashed' format within the 
+* 'view' is an angular specific process for dealing with the camelCase 
+* standard of JavaScript's naming convention.  The 'dashed' format is 
+* because the 'view' is 'html' which is 'case in-sensitive' and therefore 
+* needs to be 'processed' by angular in order for the 'view' and the 
+* JavaScript 'control' to operate together.
+*
+***************************************************************************/
+
+/*  git checkout -b angular-assignment-10-Directives-Part-2  */
